ff3a6488b7b90303013f5ff2311c5c86e65f6bf3
Елена <lenna@gmail.com>
2023-08-16 19:05:23 +0300

    Added program in Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int res_sum = SumNDigit(number);
+Console.WriteLine($"Сумма цифр в {number} = {res_sum}");
+
+
+int SumNDigit(int num)
+{
+if (num ==0) return 1;
+
+    int sum = 0;
+    while (num > 0)
+    {
+
+        sum += num % 10;
+        num /= 10;
+
+    }
+    return sum;
+
+
+}
