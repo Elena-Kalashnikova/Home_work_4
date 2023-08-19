e377a8dff4ed20bb369875618c7cda869d2df0d4
Елена <lenna@gmail.com>
2023-08-19 16:25:45 +0300

    Added program in Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+
+// Console.WriteLine($"{array}");
+
+
+void FillArray(int[] arr)
+{ Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+ 
+}
+void PrintArray(int[] arr)
+{Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]}, ");
+}
+//Console.Write($"{arr[arr.Lenght-1]}");
+Console.Write("]");
+}
